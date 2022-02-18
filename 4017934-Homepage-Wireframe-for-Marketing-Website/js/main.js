7c269da6a2ef5636d29aea1930428f789852a74d
Rascal_Two <therealrascaltwo@gmail.com>
2022-02-18 12:38:37 -0500

    Improve 4017934 accessibility

--- 4017934-Homepage-Wireframe-for-Marketing-Website/js/main.js
+++
@@ -1,6 +0,0 @@
-
-function hoverAboutUs({ currentTarget }){
-	for (const sibling of currentTarget.parentNode.children) sibling.querySelector('figure').classList.remove('dark')
-	currentTarget.querySelector('figure').classList.add('dark')
-}
-document.querySelectorAll('.about-us > section > section').forEach(section => section.addEventListener('mouseover', hoverAboutUs))
